ede327a14e780e9656098740388ea46cfd226034
krishnankrm <krm.krish@gmail.com>
2021-10-27 16:00:36 +0530

    shell tested

--- backend/runMe_linux.sh
+++ backend/runMe_linux.sh
@@ -3,10 +3,10 @@ sudo killall python3 &
 sudo killall node &
 python3 datagen.py &
 sleep 1
-node mqtt2postgres.js
+node mqtt2postgres.js &
 sleep 1
-python3 sendSMS.py
+python3 sendSMS.py &
 sleep 1
-node alertusingnode.js
+node alertusingnode.js &
 sleep 1
-node index.js
+node index.js &
