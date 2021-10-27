d4fe8ea76c6355050014272c8b5f6ddbfc21a354
Shamshudeen J <j.shamshudeen@gmail.com>
2021-10-27 12:46:31 +0530

    bat added ; not tested

---
+++ backend/runMe.bat
@@ -0,0 +1,9 @@
+start C:\Users\User\AppData\Local\Programs\Python\Python39\python.exe E:\Inspirisys\Init\AmaraRaja\software\FHS\backend\datagen.py
+timeout 1
+start C:\Program Files\nodejs\node.exe E:\Inspirisys\Init\AmaraRaja\software\FHS\backend\mqtt2postgres.js
+timeout 1
+start C:\Users\User\AppData\Local\Programs\Python\Python39\python.exe E:\Inspirisys\Init\AmaraRaja\software\FHS\backend\sendSMS.py
+timeout 1
+start C:\Program Files\nodejs\node.exe E:\Inspirisys\Init\AmaraRaja\software\FHS\backend\alertusingnode.js
+timeout 1
+start npm start
