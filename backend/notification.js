eac74b736bfc0f05e381b5a8e6513abf092146d7
kirishnan krm <85346543+krishnankrm@users.noreply.github.com>
2021-11-15 12:58:09 +0530

    Node 15/11

--- backend/notification.js
+++ backend/notification.js
@@ -7,7 +7,6 @@ const host = config1.db_cred.host;
 const password = config1.db_cred.password;
 const port = config1.db_cred.port;
 const axios=require('axios')
-console.log(user)
 const pool = new Pool({
     user: user,
     host: host,
@@ -18,8 +17,10 @@ const pool = new Pool({
 flag=0
 // while(1)
 // {
+    console.log('program started')
 async function asyncfunction()
-{   mypromise101=new Promise(async function(myResolve100, myReject100) {
+{   
+    mypromise101=new Promise(async function(myResolve100, myReject100) {
     mypromise=new Promise(async function(myResolve1, myReject) {
         pool.query(
         "Select * from alert_table where (status = true) and (lastmodified IS NULL OR lastmodified < now() - interval '1 minute')      ",
@@ -28,7 +29,6 @@ async function asyncfunction()
         })
     })
     let Alertarray=await mypromise
-    console.log(Alertarray)
     if(Alertarray.length==0)
     {
         myResolve100(1)
@@ -36,8 +36,8 @@ async function asyncfunction()
     for(let j=0;j<Alertarray.length;j++)
         {
                 let newpromise=new Promise(async (myResolve,myreject)=>{
-                    var qury="Select * from sensor_value where sensorid='"+Alertarray[j].sensorid+"'and date1 > now() - interval '1 minute'"
-                    pool.query(qury,
+                var qury="Select * from sensor_value where sensorid='"+Alertarray[j].sensorid+"'and date1 > now() - interval '1 minute'"
+                pool.query(qury,
                 async(err, res) => {
                 if(err) console.log(err)
                    if(res.rows.length!=0)
@@ -49,7 +49,6 @@ async function asyncfunction()
                                     if(parseFloat(res.rows[i].values1)<parseFloat(Alertarray[j].values1))
                                     {  var k=res.rows[i].date1.toISOString()
                                         var querystr="Insert into Notification_Table (alertid ,sensorID , operator ,values1, alertvalue , phoneNO, AlertDate, status) Values ("+Alertarray[j]['alertid']+","+Alertarray[j]['sensorid']+",'"+Alertarray[j]['operator']+"',"+res.rows[i].values1+","+Alertarray[j]['values1']+",'"+Alertarray[j]['phoneno']+"','"+k+"',TRUE)"
-                                       console.log(querystr)
                                         pool.query(
                                           querystr,
                                             (err, res1) => {
@@ -57,7 +56,6 @@ async function asyncfunction()
                                                 {   const d1 = new Date();
                                                     let date1234 = d1.toLocaleString();
                                                     var querystr2="UPDATE alert_table  SET lastmodified = '"+date1234+"' WHERE alertid= '"+Alertarray[j].alertid+"';"
-                                                    console.log(querystr2)
                                                  pool.query(
                                                        querystr2,
                                                          (err1, res2) => {
@@ -79,16 +77,36 @@ async function asyncfunction()
                                     }
                                 }
                                 else if(Alertarray[j].operator=='greaterthan')
-                                {
-                                    if(parseFloat(res.rows[i].value)>parseFloat(Alertarray[j].value))
-                                    {  
-                                        myResolve(1)
+                                {   
+                                    if(parseFloat(res.rows[i].values1)>parseFloat(Alertarray[j].values1))
+                                    {  var k=res.rows[i].date1.toISOString()
+                                        var querystr="Insert into Notification_Table (alertid ,sensorID , operator ,values1, alertvalue , phoneNO, AlertDate, status) Values ("+Alertarray[j]['alertid']+","+Alertarray[j]['sensorid']+",'"+Alertarray[j]['operator']+"',"+res.rows[i].values1+","+Alertarray[j]['values1']+",'"+Alertarray[j]['phoneno']+"','"+k+"',TRUE)"
+                                        pool.query(
+                                          querystr,
+                                            (err, res1) => {
+                                                if(!err)
+                                                {   const d1 = new Date();
+                                                    let date1234 = d1.toLocaleString();
+                                                    var querystr2="UPDATE alert_table  SET lastmodified = '"+date1234+"' WHERE alertid= '"+Alertarray[j].alertid+"';"
+                                                 pool.query(
+                                                       querystr2,
+                                                         (err1, res2) => {
+                                                             if(!err1)
+                                                             {  
+                                                                 console.log('query updated')
+                                                                 myResolve100(1)
+
+                                                             }
+                                                         })
+                                                }
+
+                                            })
                                     }
                                     else
-                                    {  
-                                        myResolve(1)
-                                    }                              
-                                  }
+                                   { 
+                                       myResolve(1)
+                                    }
+                                }
                         else
                         {
                              myResolve(1)
@@ -110,10 +128,11 @@ async function asyncfunction()
            }
         })
         let100=await(mypromise101)
-        console.log(1)
-        asyncfunction()
+        setTimeout(() => {
+            asyncfunction()
+
+        }, 300);
 
 }
-
 asyncfunction()
  
