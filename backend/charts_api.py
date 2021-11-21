aeb7009ec99f5197fe60c3ce2d0bcc1a6375b70c
Shamshudeen J <j.shamshudeen@gmail.com>
2021-11-21 22:39:13 +0530

    python runtime api tested

--- backend/charts_api.py
+++ backend/charts_api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import random
 import uvicorn
-import influxdb
+from influxdb import InfluxDBClient
 import json
 import pandas as pd
 import sqlite3
@@ -14,6 +14,8 @@ DBFILE = "AR_config.db"
 conn = sqlite3.connect(DBFILE)
 sensors_df = pd.read_sql_query("SELECT * FROM activeSensors",conn)
 topics_list = [i for i in sensors_df['sensorID']]
+client = InfluxDBClient(host='localhost',port=8086)
+client.switch_database('fhs')
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,22 +24,29 @@ app.add_middleware(
     allow_methods=["*"], # HTTP method whitelistings can be done here
     allow_headers=["*"],)
 
-
 @app.post("/runtime")
 async def runtime():
-    return_list = [random.choice(char_list) for i in range(random.randint(3,7))]
-    return_str = ""
-    for i in return_list:
-        return_str = return_str + i
-    return {"message": return_str}
+    query = """SELECT last(value) FROM "501","502","503","504","505","506","507","508"; """
+    result = client.query(query).raw['series']
+    print(result)
+    result_json = {"id":[],"ts":[],"val":[]}
+    for i in result:
+        result_json["id"].append(i["name"])
+        result_json["ts"].append(i["values"][0][0])
+        result_json["val"].append(i["values"][0][1])
+    return result_json
 
 @app.post("/charts/pressure")
 async def pressure():
-    return_list = [random.choice(char_list) for i in range(random.randint(3,7))]
-    return_str = ""
-    for i in return_list:
-        return_str = return_str + i
-    return {"message": return_str}
+    query = """SELECT mean(value) FROM "502","503","504","505","506","507","508"; """
+    result = client.query(query).raw['series']
+    print(result)
+    result_json = {"sensorid":[],"date1":[],"values1":[]}
+    for i in result:
+        result_json["sensorid"].append(i["name"])
+        result_json["date1"].append(i["values"][0][0])
+        result_json["values1"].append(i["values"][0][1])
+    return result_json
 
 @app.post("/charts/level")
 async def level():
@@ -49,4 +58,3 @@ async def level():
 
 if __name__=="__main__":
     uvicorn.run(app,host="0.0.0.0",port=PORT)
-
